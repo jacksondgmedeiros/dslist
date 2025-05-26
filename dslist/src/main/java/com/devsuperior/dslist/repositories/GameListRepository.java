31df71c1a84a166459af1ac9be3b1c79d9b021c2
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-05-25 21:26:01 -0300

    movendo os jogos de posição, finalizando o projeto da aula

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -2,6 +2,13 @@ package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+    // modifyindo é usado quando não é select, qualquer outra do crud, usa
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,34 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     // informa que não vou fazer escrita no banco, fica mais rápido nesse caso
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> listaGames = gameListRepository.findAll();
 
         return listaGames.stream().map(GameListDTO::new).toList();
+    }
+
+
+    // função para atualizar as posições da tabela de position
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        var obj =  list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        // nova forma de encontrar
+        int min = Math.min(sourceIndex, destinationIndex);
+
+        // forma ternaria da operação acima
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(),i);
+        }
     }
 
 
